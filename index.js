d7a0728bd7770c1d34249c3d8dc3ace3a79fea9b
leahbateman@gmail.com Bateman <leahbateman@gmail.com>
2018-01-24 21:26:42 +0000

    Done.

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return getElementById(nested).getElementsByClassName(target);
+  return document.querySelector("#nested .target");
 }
