20d8eaebceec53d257a2785fd83aced8586b0aa9
leahbateman@gmail.com Bateman <leahbateman@gmail.com>
2018-01-24 21:45:25 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector("#nested .target");
 }
+
+function increaseRankBy(n) {
+  const rankedListItems = document.querySelectorAll(".ranked-list li");
+  var i;
+  console.log(rankedListItems);
+  console.log(n);
+  
+  for (i = 0; i < rankedListItems.length; i++) {
+    console.log(i);
+    var innerNum = parseInt(rankedListItems[i].innerHTML);
+    console.log(innerNum);
+    innerNum = innerNum + n;
+    console.log(innerNum);
+    rankedListItems[i].innerHTML = innerNum.toString();
+  }
+}
